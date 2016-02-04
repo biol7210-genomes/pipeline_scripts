fc2e2278d866155db06d3318e31253e3f3cc3b53
Alli Gombolay <agombolay3@gmail.com>
2016-02-03 22:55:59 -0500

    Update AlignGraph.pl

--- AlignGraph.pl
+++ AlignGraph.pl
@@ -1,3 +1,7 @@
 #!/usr/bin/perl -w
 # Alli Gombolay
 # AlignGraph Assembly Pipeline
+
+use strict;
+
+AlignGraph --read1 reads_1.fa --read2 reads_2.fa --contig contigs.fa --genome genome.fa --distanceLow 100 --distanceHigh 1500 --extendedContig extendedContigs.fa --remainingContig remainingContigs.fa
