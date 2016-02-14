64b73099bcb998dd77d2df292a6c89b1115bbcb1
Aroon Chande <github@atc.io>
2016-02-14 10:18:00 -0500

    QUAST report generation, general fixes

--- pipeline_termreport.pl
+++ pipeline_termreport.pl
@@ -7,16 +7,20 @@
 use strict;
 use Getopt::Long;
 use File::Basename;
+use File::Temp;
 use Pod::Usage;
 use Term::Report;
+use Async;
 my $prog = basename($0);
 if (@ARGV < 1){print_usage();exit 1;}
-my($inDir,$outDir,$i,$base,$out,$assemblyStatus,$currentStatus,$r1,$r2,$R,$refDir,@steps);
-GetOptions ('o=s' => \$outDir, 'in=s' => \$inDir, 'R=s' => \$refDir, 'steps=s{1,}' => \@steps);
+my($inDir,$outDir,$i,$base,$out,$assemblyStatus,$currentStatus,$r1,$r2,$R,$ref,@steps,$link,$quast);
+GetOptions ('o=s' => \$outDir, 'in=s' => \$inDir, 'R=s' => \$ref, 'steps=s{1,}' => \@steps);
+if (grep(/,/, @steps)){@steps = split(/,/,join(',',@steps));}
 die print_usage() unless ((defined $outDir) && (defined $inDir));
-# Remove generalization and make assumptions about naming scheme
+# Multiplier for main progress bar
 my $stepMult = scalar @steps;
-$stepMult++ if (defined grep(/abyss/i, @steps));
+$stepMult++ if (grep(/abyss/i, @steps));
+# Remove generalization and make assumptions about naming scheme
 my @infiles = glob ( "$inDir/*R1_001_val_1.fq.gz" );
 my $max = @infiles;
 my $amax = $max * $stepMult;
@@ -24,41 +28,74 @@ my $amax = $max * $stepMult;
 initialize_bar();
 $assemblyStatus->start;
 $currentStatus -> start;
-update_bar("SPAdes progress:","1");
+system(`mkdir -p $outDir/contigs`);
 #SPAdes
-foreach (@infiles){
+if (grep(/spades/i, @steps)){
+	update_bar("SPAdes progress:","1");
+	foreach (@infiles){
 	get_files($_);
 	$currentStatus->subText("Running SPAdes on $out");
-#	system(`mkdir -p $outDir/spades/$out`);
-#	system(`spades.py -h -1 $r1 -2 $r2 -o $outDir/spades/$out -k 99,115,127 --careful 2>$outDir/$out/spades.log`);
+	system(`mkdir -p $outDir/spades/$out`);
+	system(`spades.py -1 $r1 -2 $r2 -o $outDir/spades/$out -k 99,113,127 --only-assembler &>$outDir/status.log`);
+	$link = join(".","spades",$out,"fa");
+	system(`cp $outDir/spades/$out/contigs.fasta $outDir/contigs/$link`);
+	system(`rm -rf $outDir/spades/$out/corrected`);
 	sleep 1;
 	$assemblyStatus->update();
 	$currentStatus->update();
 }
+	$quast =  Async->new( sub {system(`quast -R $ref --threads=2 $outDir/contigs/spades* -o $outDir/quast/spades >/dev/null`)} or die);
+}
 # Velvet
-update_bar("Velvet progress:","1");
-foreach (@infiles){
+
+if (grep(/velvet/i, @steps)){
+	update_bar("Velvet progress:","1");
+	foreach (@infiles){
 	get_files($_);
-	$currentStatus -> label("Velvet progress: ");
-#	system(`mkdir -p $outDir/velvet/`);
-#	system(`VelvetOptimiser.pl -d $outDir/velvet/$out/ -s 97 -e 127 -x 10 -f '-fastq.gz -shortPaired -separate $r1 $r2' -t 2 --optFuncKmer 'n50'`);
 	$currentStatus->subText("Running Velvet on $out");
+	system(`mkdir -p $outDir/velvet/`);
+	system(`VelvetOptimiser.pl -d $outDir/velvet/$out/ -s 97 -e 127 -x 10 -f '-fastq.gz -shortPaired -separate $r1 $r2' -t 6 --optFuncKmer 'n50' &>$outDir/status.log`);
+	$link = join(".","velvet",$out,"fa");
+	system(`cp $outDir/velvet/$out/contigs.fa $outDir/contigs/$link`);
+	system(`rm -rf $outDir/velvet/$out/Sequences`);
 	sleep 1;
 	$assemblyStatus->update();
 	$currentStatus->update();	
 }
+	$quast =  Async->new( sub {system(`quast -R $ref --threads=2 $outDir/contigs/velvet* -o $outDir/quast/velvet >/dev/null`)} or die);
+}
 # ABySS
-update_bar("ABySS progress:","2");
-foreach (@infiles){
-	for my $kmer ("97","115"){
-        get_files($_);
-    	$currentStatus->subText("Running ABySS on $out with k of $kmer");
-#		system(`mkdir -p $outDir/abyss/k$kmer`);
-#		system("abyss-pe -C $outDir/abyss/k$kmer k=$kmer name=$out in="$r1 $r2" j=4");
-       	sleep 1;
-		$currentStatus->update();
-       	$assemblyStatus->update();
+if (grep(/abyss/i, @steps)){
+ 	update_bar("ABySS progress:","2");
+	foreach (@infiles){
+		for my $kmer ("97","115"){
+			get_files($_);
+			$currentStatus->subText("Running ABySS on $out with k of $kmer");
+			system(`mkdir -p $outDir/abyss/k$kmer`);
+			system("abyss-pe -C $outDir/abyss/k$kmer k=$kmer name=$out in='$r1 $r2' j=6 &>$outDir/status.log ");
+			$link = join(".","abyss",$out,"fa");
+			my $contig = join("-",$out,"contigs.fa");
+			system(`cp $outDir/abyss/k$kmer/$contig $outDir/contigs/$link`);
+			sleep 1;
+			$currentStatus->update();
+			$assemblyStatus->update();
+		}
 	}
+	$quast =  Async->new( sub {system(`quast -R $ref --threads=2 $outDir/contigs/abyss* -o $outDir/quast/abyss &>/dev/null`)} or die);
+}
+print "\n\n\n\n\nWaiting on QUAST\n\n";
+while (1){
+	if ($quast->ready){
+		#combine reports
+		my $report=temp_filename();
+		open REPORT, ">$report" or die "Cannot open $report: $!";
+		print REPORT "Assembly\n# contigs (>= 0 bp)\n# contigs (>= 1000 bp)\n# contigs (>= 5000 bp)\n# contigs (>= 10000 bp)\n# contigs (>= 25000 bp)\n# contigs (>= 50000 bp)\nTotal length (>= 0 bp)\nTotal length (>= 1000 bp)\nTotal length (>= 5000 bp)\nTotal length (>= 10000 bp)\nTotal length (>= 25000 bp)\nTotal length (>= 50000 bp)\n# contigs\nLargest contig\nTotal length\nReference length\nGC (%)\nReference GC (%)\nN50\nNG50\nN75\nNG75\nL50\nLG50\nL75\nLG75\n# misassemblies\n# misassembled contigs\nMisassembled contigs length\n# local misassemblies\n# unaligned contigs\nUnaligned length\nGenome fraction (%)\nDuplication ratio\n# Ns per 100 kbp\n# mismatches per 100 kbp\n# indels per 100 kbp\nLargest alignment\nNA50\nNGA50\nNA75\nNGA75\nLA50\nLGA50\nLA75\nLGA75";
+		close REPORT;
+		system(`paste $report $outDir/quast/*/report.tsv| cut -f 1,3,5,7 > $outDir/quast/final_report.tsv`);
+		print "Assembly quality scores can be found at: $outDir/quast/final_report.tsv\n";
+		exit 0;
+	}
+	sleep 1;
 }
 
 exit 0;
@@ -114,7 +151,35 @@ sub get_files(){
 	$r2 = join('_',$base,"R2_001_val_2.fq.gz");
 }
 
-sub run_quast(){
-		
-}	
+sub temp_filename{
+	    my $file = File::Temp->new(
+	        TEMPLATE => 'tempXXXXX',
+	        DIR      => '/tmp/',
+	    );
+	}
 
+sub print_usage{
+    warn <<"EOF";
+
+USAGE
+  $prog -in <indir> -o <outdir> -R <reffile> --steps <steps,to,run>
+
+DESCRIPTION
+	Spades pipeline
+
+OPTIONS
+	-in	dir		Directory with fq.gz
+	-o	dir		output folder
+	-R	file	Reference genome file
+	--steps	list	Comma separated list of steps to run
+					Valid steps: abyss, velvet, spades
+EXAMPLES
+  $prog -in ./scratch/reads -o ./scratch/assemblies --steps velvet,abyss,spades -R ./reference/genomic.fna.gz
+  $prog -h
+
+EXIT STATUS
+  0     Successful completion
+  >0    An error occurred
+
+EOF
+}
