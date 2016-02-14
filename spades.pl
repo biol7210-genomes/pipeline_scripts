25a6240f8e48e6b440dedcb14ffccdc04bdd0d98
Aroon Chande <github@atc.io>
2016-02-14 10:28:23 -0500

    spades

--- spades.pl
+++ spades.pl
@@ -13,18 +13,12 @@ use Term::ProgressBar;
 my $prog = basename($0);
 if (@ARGV < 1){print_usage();exit 1;}
 my($inDir,$outDir,$i,$base,$out);
-<<<<<<< HEAD
-GetOptions ('o=s' => \$outDir, 'in=s' => \$inDir);
-die print_usage() unless ((defined $outDir) && (defined $inDir));
-my @infiles = glob ( "$inDir/*.fq.gz" );
-=======
 my $j=0;
 GetOptions ('o=s' => \$outDir, 'in=s' => \$inDir);
 die print_usage() unless ((defined $outDir) && (defined $inDir));
 my @infiles = glob ( "$inDir/*.fq.gz" );
 my $max =  @infiles/2;
 my $spades_progress = Term::ProgressBar->new ({count => $max, name => 'SPAdes progress', term_width => '80', ETA   => 'linear',});
->>>>>>> refs/remotes/biol7210-genomes/aroon
 for ($i = 0; $i < @infiles; $i += 2){
 	$j++;
 	$base = $infiles[$i];
@@ -33,14 +27,9 @@ for ($i = 0; $i < @infiles; $i += 2){
 	system(`mkdir -p $outDir/$out`);
 	my $r1 = join('_',$base,"R1_001_val_1.fq.gz");
 	my $r2 = join('_',$base,"R2_001_val_2.fq.gz");
-<<<<<<< HEAD
-	print STDERR "Running SPAdes with $r1 and $r2\n";
-	system(`spades.py -1 $r1 -2 $r2 -o $outDir/$out -k 21,33,55,77,99,127 --careful 2>$outDir/$out/spades.log`);
-=======
 	system("spades.py -1 $r1 -2 $r2 -o $outDir/$out -k 97,115,127 --careful 2>$outDir/$out/spades.log");
         $spades_progress->update($j);
 	sleep 1;
->>>>>>> refs/remotes/biol7210-genomes/aroon
 }
 
 exit 0;
@@ -56,7 +45,7 @@ DESCRIPTION
 
 OPTIONS
   -in	dir		Directory with FASTQ
-  -out  dir		output files
+  -out  dir		outpur files
 
 EXAMPLES
   $prog -in ../reads/ ./spades_out
