a8ef7911336269df7a6aeb53d147c168454da33c
Aroon Chande <ar0ch@users.noreply.github.com>
2016-04-18 11:41:13 -0400

    No longer needed

--- sample_fq.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-for i in 
-
-fq_count=`calc "$(wc -l < all_reads.fastq) / 8"`
-samples=`calc "floor($fq_count / 3)"`
-sample --lines-per-offset=8 --sample-size=${samples} all_reads.fastq > random_sample.fastq
